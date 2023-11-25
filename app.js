632ee04ab87bbe3390b391ed65c337e514696c83
AnatoliiGoIt <troloko8@gmail.com>
2023-11-26 00:24:19 +0200

    finished authentication section

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
+const hpp = require('hpp')
 
 const AppError = require(`./utils/AppError`)
 const errorHandler = require('./controllers/errorController')
@@ -8,14 +13,44 @@ const userRouter = require(`./routes/userRoute`)
 
 const app = express()
 
-// 1)MIDDLWARES
+// 1)GLOBAL MIDDLWARES
+    // Set security HTTP headers
+app.use(helmet())
+    // Development log in
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev')) // for more info to terminal
 }
 
-app.use(express.json())
-app.use(express.static(`${__dirname}/public`)) // sets up a new root folder for URL row // work for static files
+    // limiter for max request from one IP
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: "Too many requests from this IP please try again in an hour!"
+})
+app.use('/api', limiter)
 
+    // Body parser, reading data from body into req.body 
+app.use(express.json({limit: '10kb'})) // body size limiter
+
+    // DATA sanitization against NoSQL query injection
+app.use(mongoSanitize())
+
+    // Data sanitization against XSS
+app.use(xss())
+    // Prevent parametr polution
+app.use(hpp({
+    whitelist: [
+        'duration', 
+        'ratingAverage', 
+        'ratingsQuantity', 
+        'maxGroupSize', 
+        'difficulty', 
+        'price'
+    ]
+}))
+    // Serving static files
+app.use(express.static(`${__dirname}/public`)) // sets up a new root folder for URL row // work for static files
+    // Test middlware
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString()
     next()
