39511a0f9982611f57d59b8f51c96d0f2d7c8564
AnatoliiGoIt <119327888+troloko1001@users.noreply.github.com>
2023-12-25 23:51:53 +0200

    m

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
 const rateLimit = require('express-rate-limit')
@@ -14,50 +15,63 @@ const reviewRouter = require(`./routes/reviewRoute`)
 
 const app = express()
 
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
+
+// Serving static files
+// app.use(express.static(`${__dirname}/public`)) // sets up a new root folder for URL row // work for static files
+app.use(express.static(path.join(__dirname, 'public'))) // sets up a new root folder for URL row // work for static files
+
 // 1)GLOBAL MIDDLWARES
-    // Set security HTTP headers
+// Set security HTTP headers
 app.use(helmet())
-    // Development log in
+// Development log in
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev')) // for more info to terminal
 }
 
-    // limiter for max request from one IP
+// limiter for max request from one IP
 const limiter = rateLimit({
     max: 100,
     windowMs: 60 * 60 * 1000,
     message: "Too many requests from this IP please try again in an hour!"
 })
+
 app.use('/api', limiter)
 
-    // Body parser, reading data from body into req.body 
-app.use(express.json({limit: '10kb'})) // body size limiter
+// Body parser, reading data from body into req.body 
+app.use(express.json({ limit: '10kb' })) // body size limiter
 
-    // DATA sanitization against NoSQL query injection
+// DATA sanitization against NoSQL query injection
 app.use(mongoSanitize())
 
-    // Data sanitization against XSS
+// Data sanitization against XSS
 app.use(xss())
-    // Prevent parametr polution
+// Prevent parametr polution
 app.use(hpp({
     whitelist: [
-        'duration', 
-        'ratingAverage', 
-        'ratingsQuantity', 
-        'maxGroupSize', 
-        'difficulty', 
+        'duration',
+        'ratingAverage',
+        'ratingsQuantity',
+        'maxGroupSize',
+        'difficulty',
         'price'
     ]
 }))
-    // Serving static files
-app.use(express.static(`${__dirname}/public`)) // sets up a new root folder for URL row // work for static files
-    // Test middlware
+
+// Test middlware
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString()
     next()
 })
 
 // 3) ROUTES
+app.use('/', (req, res) => {
+    res
+        .status(200)
+        .render('base')
+})
+
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/reviews', reviewRouter)
