0f82a6e56625ffc1a6dc7104284e1c971389f14e
AnatoliiGoIt <119327888+troloko1001@users.noreply.github.com>
2024-06-22 15:31:16 +0300

    did set up the settings for uploding tour images to DB

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -45,11 +45,38 @@ exports.uploadTourImages = upload.fields([
 // upload.single('image') // - req.file
 // upload.array('images', 5) // - req.files
 
-exports.resizeTourImages= (req, res, next) => {
-    console.log(req.files)
+exports.resizeTourImages = catchAsync(async (req, res, next) => {
+    if (!req.files.images || !req.files.imageCover) return next()
+
+    //1) Cover image
+    req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`
+
+    await sharp(req.files.imageCover[0].buffer)
+        .resize(2000, 1333)
+        .toFormat('jpeg')
+        .jpeg({quality: 90})
+        .toFile(`public/img/tours/${req.body.imageCover}`)
+    
+
+    // 2) Images
+    req.body.images = []
+
+    await Promise.all(
+        req.files.images.map(async (file, index) => {
+            const filename = `tour-${req.params.id}-${Date.now()}-${++index}.jpeg`
+
+            await sharp(file.buffer)
+                .resize(2000, 1333)
+                .toFormat('jpeg')
+                .jpeg({quality: 90})
+                .toFile(`public/img/tours/${filename}`)
+    
+            req.body.images.push(filename)
+        })
+    )
 
     next()
-}
+})
 
 exports.checkBody = (req, res, next) => {
     if (((req.body.name ?? '') === '') || ((req.body.price ?? 0) === 0)) {
